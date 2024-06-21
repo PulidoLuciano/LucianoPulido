037b815b8d2cee62a2195240a7054f965c86d784
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-06-21 20:10:33 -0300

    Fix responsive on footer

--- Frontend/src/components/footer.tsx
+++ Frontend/src/components/footer.tsx
@@ -4,15 +4,15 @@ import linkedInIcon from "../assets/linkedIn.svg"
 
 export default function Footer(){
     return(
-        <footer className="absolute bg-primary-dark bottom-0 w-full">
-            <div className="max-w-screen-laptopL mx-auto flex justify-between items-center">
-                <h1 className="font-bold text-tertiary select-none">Luciano Pulido</h1>
+        <footer className="absolute bg-primary-dark bottom-0 w-full z-0">
+            <div className="max-w-screen-laptopL mx-auto flex justify-between items-center flex-col gap-4 laptop:flex-row">
+                <h1 className="font-bold text-tertiary select-none pt-2 laptop:p-0 laptop:pl-4">Luciano Pulido</h1>
                 <div className="flex gap-4">
                     <a href="x.com"><img src={xIcon} alt="X's icon" className="size-8 hover:brightness-110"/></a>
                     <a href="github.com"><img src={githubIcon} alt="GitHub's icon" className="size-8 hover:brightness-110"/></a>
                     <a href="linkedIn.com"><img src={linkedInIcon} alt="LinkedIn's icon" className="size-8 hover:brightness-110"/></a>
                 </div>
-                <nav className="bg-primary-dark text-tertiary p-4">
+                <nav className="bg-primary-dark text-tertiary pb-2 laptop:p-4">
                     <div className="flex gap-6">
                         <a href="/" className="hover:text-white">About</a>
                         <a href="" className="hover:text-white">Contact</a>
