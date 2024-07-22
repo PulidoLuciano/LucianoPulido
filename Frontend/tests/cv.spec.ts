79fffbc68231ce6b55fc36947bad5bacfe771747
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-07-22 00:58:56 -0300

    Add form validation tests

--- Frontend/tests/cv.spec.ts
+++ Frontend/tests/cv.spec.ts
@@ -105,5 +105,56 @@ test.describe("Main page", () => {
         test("Has contact section", async ({page}) => {
             await expect(page.getByRole("heading", {name: "Contact me!"})).toBeVisible();
         });
-    })
+    });
+
+    test.describe("Form validation", () => {
+        test("Show error messages when fields are empty", async ({page}) => {
+            await page.getByRole("button", {name: "Send"}).click();
+            await expect(page.getByText("A name is required", {exact: false})).toBeVisible();
+            await expect(page.getByText("An email is required", {exact: false})).toBeVisible();
+            await expect(page.getByText("A subject is required", {exact: false})).toBeVisible();
+            await expect(page.getByText("A message is required", {exact: false})).toBeVisible();
+        });
+
+        test("Show successful message when send and empty inputs", async ({page}) => {
+            const nameInput = await page.getByLabel("Your name");
+            await nameInput.fill("John Snow");
+            const emailInput = await page.getByLabel("Your e-mail address");
+            await emailInput.fill("JohnSnow@gmail.com");
+            const subjectInput = await page.getByLabel("Subject");
+            await subjectInput.fill("Conquer the north");
+            const messageInput = await page.getByLabel("Message");
+            await messageInput.fill("Hey, I want to defeat the White walkers with you!");
+            await page.getByRole("button", {name: "Send"}).click();
+            await expect(page.getByText("Message sended successfully", {exact: false}));
+            await expect(nameInput).toBeEmpty();
+            await expect(emailInput).toBeEmpty();
+            await expect(subjectInput).toBeEmpty();
+            await expect(messageInput).toBeEmpty();
+        });
+
+        test("Show error messages when fields excede length", async ({page}) => {
+            const nameInput = await page.getByLabel("Your name");
+            await nameInput.fill("John SnowJohn SnowJohn SnowJohn SnowJohn SnowJohn SnowJohn SnowJohn Snow");
+            const emailInput = await page.getByLabel("Your e-mail address");
+            await emailInput.fill("JohnSnow@gmail.comJohnSnow@gmail.comJohnSnow@gmail.comJohnSnow@gmail.comJohnSnow@gmail.com");
+            const subjectInput = await page.getByLabel("Subject");
+            await subjectInput.fill("Conquer the northConquer the northConquer the north");
+            const messageInput = await page.getByLabel("Message");
+            await messageInput.fill("Hey, I want to defeat the White walkers with you!Hey, I want to defeat the White walkers with you!Hey, I want to defeat the White walkers with you!Hey, I want to defeat the White walkers with you!Hey, I want to defeat the White walkers with you!Hey, I want to defeat the White walkers with you!Hey, I want to defeat the White walkers with you!");
+            await page.getByRole("button", {name: "Send"}).click();
+            await expect(page.getByText("Name must not excede 30 characters", {exact: false})).toBeVisible();
+            await expect(page.getByText("E-mail must not excede 50 characters", {exact: false})).toBeVisible();
+            await expect(page.getByText("Subject must not excede 30 characters", {exact: false})).toBeVisible();
+            await expect(page.getByText("Message must not excede 300 characters", {exact: false})).toBeVisible();
+            await expect(nameInput).toHaveValue("John SnowJohn SnowJohn SnowJohn SnowJohn SnowJohn SnowJohn SnowJohn Snow");
+        });
+
+        test("Show error if e-mail does not match format", async ({page}) => {
+            const emailInput = await page.getByLabel("Your e-mail address");
+            await emailInput.fill("JohnSnow.com");
+            await page.getByRole("button", {name: "Send"}).click();
+            await expect(page.getByText("That does not seems like an e-mail", {exact: false})).toBeVisible();
+        });
+    });
 })
