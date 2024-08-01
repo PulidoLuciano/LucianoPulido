50d94f85b2ff62e380a66bd127336e01f3faa5ce
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-08-01 16:27:03 -0300

    Add scroll buttons in carousel

--- Frontend/src/components/forum/carrousel.tsx
+++ Frontend/src/components/forum/carrousel.tsx
@@ -1,6 +1,7 @@
 import { Link } from "luciano-react-router";
 import ArticleLink from "./articleLink";
 import moreIcon from "../../assets/moreRight.svg"
+import { useRef, useState } from "react";
 
 export interface ArticlePreview{
     title : string
@@ -16,6 +17,39 @@ interface CarrouselProps{
 }
 
 export default function ArticlesCarrousel({title, articles, categoryRef} : CarrouselProps){
+    
+    const contentDiv = useRef(null);
+    const position = useRef(0);
+    const [rightVisible, setRightVisible] = useState(true);
+    const [leftVisible, setLeftVisible] = useState(false);
+
+    function scrollToNext(){
+        if(!contentDiv.current) return;
+        if(!leftVisible) setLeftVisible(true);
+        position.current++;
+        scrollTo(position.current);
+        if((contentDiv.current as HTMLDivElement).scrollLeft + (contentDiv.current as HTMLDivElement).offsetWidth >= (contentDiv.current as HTMLDivElement).scrollWidth){
+            setRightVisible(false);
+            return;
+        }
+    }
+
+    function scrollBehind(){
+        if(!contentDiv.current) return;
+        if(!rightVisible) setRightVisible(true);
+        position.current--;
+        scrollTo(position.current);
+        if((contentDiv.current as HTMLDivElement).scrollLeft <= 0){
+            setLeftVisible(false);
+            return;
+        }
+    }
+
+    function scrollTo(position : number){
+        if(!contentDiv.current) return;
+        (contentDiv.current as HTMLDivElement).scroll({left: position * 256, behavior: "smooth"});
+    }
+    
     return(
         <article className="w-full mb-8 group">
             <div className="grid grid-cols-[auto_1fr] items-center mb-4">
@@ -23,8 +57,10 @@ export default function ArticlesCarrousel({title, articles, categoryRef} : Carro
                 <div className="hidden laptop:block h-1 w-full bg-primary-light relative -bottom-1"/>
             </div>
             <div className="relative">
-                <button className="hidden absolute w-12 h-12 text-2xl justify-center items-center font-bold bg-secondary-light rounded-full -left-6 laptop:group-hover:flex top-1/3 shadow-md shadow-black">{"<"}</button>
-                <div className="flex flex-col overflow-x-hidden items-center tablet:flex-row tablet:items-start gap-4 py-4">
+                {
+                    (leftVisible) ? <button className="hidden absolute w-12 h-12 text-2xl justify-center items-center font-bold bg-secondary-light rounded-full -left-6 laptop:group-hover:flex top-1/3 shadow-md shadow-black" onClick={scrollBehind}>{"<"}</button> : null
+                }
+                <div className="flex flex-col overflow-x-hidden items-center tablet:flex-row tablet:items-start gap-4 py-4" ref={contentDiv}>
                     {
                         articles.map(article => {
                             return <ArticleLink article={article}/>
@@ -35,7 +71,10 @@ export default function ArticlesCarrousel({title, articles, categoryRef} : Carro
                         <p className="text-lg font-semibold text-pretty">View more</p>
                     </Link>
                 </div>
-                <button className="hidden absolute w-12 h-12 text-2xl justify-center items-center font-bold bg-secondary-light rounded-full -right-6 laptop:group-hover:flex top-1/3 shadow-md shadow-black">{">"}</button>
+                {
+                    (rightVisible) ? 
+                    <button className="hidden absolute w-12 h-12 text-2xl justify-center items-center font-bold bg-secondary-light rounded-full -right-6 laptop:group-hover:flex top-1/3 shadow-md shadow-black" onClick={scrollToNext}>{">"}</button> : null
+                }
             </div>
         </article>
     )
