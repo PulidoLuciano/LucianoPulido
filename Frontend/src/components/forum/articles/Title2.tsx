cade83dd6407f834804b862b4b667e09a7bf30fd
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-08-14 17:19:47 -0300

    Add padding to titles

--- Frontend/src/components/forum/articles/Title2.tsx
+++ Frontend/src/components/forum/articles/Title2.tsx
@@ -1,3 +1,3 @@
 export default function Title2({text} : {text : string}){
-    return <h3 className="text-3xl text-pretty font-semibold">{text}</h3>
+    return <h3 className="text-3xl text-pretty font-semibold pb-2">{text}</h3>
 }
