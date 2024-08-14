68cc53fc46cf6f7b4f43f62cf7e290e7e943fefe
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-08-14 17:34:01 -0300

    Add styles to danger

--- Frontend/src/components/forum/articles/danger.tsx
+++ Frontend/src/components/forum/articles/danger.tsx
@@ -1,3 +1,5 @@
 export default function Danger({text} : {text : string}){
-    return <div>{text}</div>
+    return <div className="bg-red-300 my-4 border-4 py-4 rounded border-red-600 flex justify-center">
+        <span>⚠ {text}</span>
+    </div>
 }
