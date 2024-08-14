c14bbae9cd048bac5b643d5ed40117095f130fd2
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-08-14 17:34:21 -0300

    Add styles to warning

--- Frontend/src/components/forum/articles/warning.tsx
+++ Frontend/src/components/forum/articles/warning.tsx
@@ -1,3 +1,5 @@
 export default function Warning({text} : {text : string}){
-    return <div>{text}</div>
+    return <div className="bg-yellow-300 my-4 border-4 py-4 rounded border-yellow-600 flex justify-center">
+        <span>⚠ {text}</span>
+    </div>
 }
