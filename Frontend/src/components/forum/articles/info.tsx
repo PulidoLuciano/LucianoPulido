6273ca0ac97ff37d111c4fe882021cd35e12e79a
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-08-14 17:34:09 -0300

    Add styles to info

--- Frontend/src/components/forum/articles/info.tsx
+++ Frontend/src/components/forum/articles/info.tsx
@@ -1,3 +1,5 @@
 export default function Info({text} : {text : string}){
-    return <div>{text}</div>
+    return <div className="bg-blue-300 my-4 border-4 py-4 rounded border-blue-600 flex justify-center">
+        <span>🛈 {text}</span>
+    </div>
 }
