9b2d38994352851b660794d132ab4e98f47d65d1
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-08-16 11:22:33 -0300

    Add formatting to code snippets

--- Frontend/src/components/forum/articles/code.tsx
+++ Frontend/src/components/forum/articles/code.tsx
@@ -1,7 +1,16 @@
+import Prism from 'prismjs';
+import { useEffect } from 'react';
+import 'prismjs/themes/prism-tomorrow.min.css'
+
 export default function Code({text, language} : {text : string, language : string}){
+    
+    useEffect(() => {
+        Prism.highlightAll();
+    }, []);
+    
     return (
-        <pre>
-            <code slot="">{text}</code>
+        <pre className='py-6'>
+            <code slot='' className={`language-${language}`}>{text}</code>
         </pre>
     )
 }
--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import prismjsPlugin from 'vite-plugin-prismjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), 
+    prismjsPlugin({
+      languages: ["javascript"],
+      theme: "tomorrow",
+    })
+  ],
 })
