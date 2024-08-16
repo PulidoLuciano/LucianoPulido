08df34aeb7b3e101f980c2039864f84113cf23de
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-08-16 11:22:10 -0300

    Add key to li

--- Frontend/src/components/forum/articles/ordered.tsx
+++ Frontend/src/components/forum/articles/ordered.tsx
@@ -1,3 +1,3 @@
 export default function Ordered({items} : {items : string[]}){
-    return <ol className="list-decimal list-inside pl-6">{items.map(item => <li>{item}</li>)}</ol>
+    return <ol className="list-decimal list-inside pl-6">{items.map((item, index) => <li key={index}>{item}</li>)}</ol>
 }
--- Frontend/src/components/forum/articles/unordered.tsx
+++ Frontend/src/components/forum/articles/unordered.tsx
@@ -1,3 +1,3 @@
 export default function Unordered({items} : {items : string[]}){
-    return <ul className="list-disc list-inside pl-6">{items.map(item => <li>{item}</li>)}</ul>
+    return <ul className="list-disc list-inside pl-6">{items.map((item, index) => <li key={index}>{item}</li>)}</ul>
 }
