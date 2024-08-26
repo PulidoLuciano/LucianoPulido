1d9d25bd92527a5255278a67ae49025ec7ed11ad
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-08-25 23:50:56 -0300

    Set up initial app

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,1 +1,17 @@
-console.log("Testing");
+import "dotenv/config"
+import "express"
+
+require("dotenv").config()
+const express = require("express");
+
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (request, response) => { 
+    response.status(200).send("Hello World");
+  }); 
+
+app.listen(port, () => {
+    console.log(`Server listening on http://localhost:${port}`);
+})
