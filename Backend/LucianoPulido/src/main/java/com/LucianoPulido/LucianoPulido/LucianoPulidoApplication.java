c87f17a9ac0f638b3724eea77f0cea7d7bcfafe4
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-12-14 12:54:03 -0300

    Initialize java backend with spring

---
+++ Backend/LucianoPulido/src/main/java/com/LucianoPulido/LucianoPulido/LucianoPulidoApplication.java
@@ -0,0 +1,13 @@
+package com.LucianoPulido.LucianoPulido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucianoPulidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucianoPulidoApplication.class, args);
+	}
+
+}
---
+++ Backend/LucianoPulido/src/test/java/com/LucianoPulido/LucianoPulido/LucianoPulidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LucianoPulido.LucianoPulido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucianoPulidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
