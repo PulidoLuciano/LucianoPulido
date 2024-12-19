f7af17b22d7670fddd78c64a8a8ea51f185d1bac
Luciano Pulido <lucianonicolaspulido@gmail.com>
2024-12-19 15:05:13 -0300

    Add controller annotations

--- Backend/LucianoPulido/src/main/java/com/LucianoPulido/LucianoPulido/controllers/routes/implementations/ArticleController.java
+++ Backend/LucianoPulido/src/main/java/com/LucianoPulido/LucianoPulido/controllers/routes/implementations/ArticleController.java
@@ -1,11 +1,16 @@
 package com.LucianoPulido.LucianoPulido.controllers.routes.implementations;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.LucianoPulido.LucianoPulido.controllers.data.dto.ArticleDTO;
 import com.LucianoPulido.LucianoPulido.controllers.data.mappers.ArticleMapper;
 import com.LucianoPulido.LucianoPulido.controllers.routes.base.GenericController;
 import com.LucianoPulido.LucianoPulido.models.Article;
 import com.LucianoPulido.LucianoPulido.services.interfaces.ArticlesService;
 
+@RestController
+@RequestMapping("/article")
 public class ArticleController extends GenericController<Article, String, ArticlesService, ArticleDTO, ArticleMapper>{
 
 }
