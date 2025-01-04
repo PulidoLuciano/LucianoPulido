30cb925f094928d28eb19665a130750f023b16a3
Luciano Pulido <lucianonicolaspulido@gmail.com>
2025-01-04 17:53:29 -0300

    Fix infinite api call loop in refresh

--- Frontend/src/hooks/useFetch.tsx
+++ Frontend/src/hooks/useFetch.tsx
@@ -5,14 +5,18 @@ function useFetch(){
     
     const auth = useAuth();
 
-    async function fetcher(route : string, options : RequestInit){
+    async function fetcher(route : string, options : RequestInit, retry = false){
         const response = await fetch( applicationConstants.VITE_API_BASE_URL + route , options);
         const data = await response.json();
         if(response.ok){
             return data;
         }else if(response.status === 401){
-            await auth.getNewRefreshToken();
-            fetcher(route, options);
+            if(!retry){
+                await auth.getNewRefreshToken();
+                fetcher(route, options, true);
+            }else{
+                throw new Error(data.error);
+            }
         }else{
             throw new Error(data.error);
         }
