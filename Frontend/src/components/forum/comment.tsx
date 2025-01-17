ceeb97dbd594ea05b6532ab0bc9f8256d601a000
Luciano Pulido <lucianonicolaspulido@gmail.com>
2025-01-17 12:52:28 -0300

    Add form to post responses

--- Frontend/src/components/forum/comment.tsx
+++ Frontend/src/components/forum/comment.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
 import { useFetch } from "../../hooks/useFetch";
 import { CommentData } from "../../types";
+import { Form } from "pulido-react-form";
 
 export default function Comment( { data } : { data : CommentData}){
     
     const { fetcher } = useFetch();
     const [ responses, setResponses ] = useState<Array<CommentData>>([]);
+    const [ showForm, setShowForm ] = useState<Boolean>(false);
     
     useEffect(() => {
         async function fetchComments(){
@@ -15,6 +17,15 @@ export default function Comment( { data } : { data : CommentData}){
 
         fetchComments();
     }, []);
+
+    async function postResponse(event : React.SyntheticEvent<HTMLFormElement>) {
+      event.preventDefault();
+      const dataResponse = { message : ((event.target as HTMLFormElement)[0] as HTMLInputElement).value };
+      const newResponse = await fetcher(`/comment/${data.id}/response`, "POST", { body: JSON.stringify(dataResponse) });
+      setResponses([newResponse, ...responses]);
+      (event.target as HTMLFormElement).reset();
+      setShowForm(false);
+    }
     
     return(
         <article className="pl-3 border-l-2 hover:border-primary-light">
@@ -25,9 +36,20 @@ export default function Comment( { data } : { data : CommentData}){
           <p>
             {data.message}
           </p>
-          <button className="text-secondary-light w-full text-right underline">
-            Answer
-          </button>
+          {
+            (!showForm) ?
+              <button className="text-secondary-light w-full text-right underline" onClick={() => setShowForm(true)}>
+                Answer
+              </button>
+            :
+              <Form className="my-2 w-full flex flex-col gap-2 items-end" onSubmit={postResponse}>
+                <input type="text" placeholder="Write your response here!" className="w-11/12 bg-transparent border-b-2 border-primary-light rounded-sm outline-none"/>
+                <div className="w-full flex justify-end gap-2">
+                  <button type="reset" className="text-primary-dark-light underline" onClick={() => setShowForm(false)}>Cancel</button>
+                  <button type="submit" className="text-secondary-light underline">Submit</button>
+                </div>
+              </Form>
+          }
           {
             responses.map(response => <Comment data={response}/>)
           }
