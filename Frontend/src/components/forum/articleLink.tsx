7ca3347dc1a0659ceac464c3f533fa1aa49cc356
Luciano Pulido <lucianonicolaspulido@gmail.com>
2025-01-18 19:12:10 -0300

    Fix height in article's links

--- Frontend/src/components/forum/articleLink.tsx
+++ Frontend/src/components/forum/articleLink.tsx
@@ -3,7 +3,7 @@ import { ArticleShortPreview } from "../../types";
 
 export default function ArticleLink({article} : {article : ArticleShortPreview}){
     return(
-        <Link href={`/articles/${article.url}`} className="inline-block w-60 h-60">
+        <Link href={`/articles/${article.url}`} className="inline-block w-60 h-auto">
             <img src={article.imageUrl} alt="" className="max-w-full min-w-60 aspect-video"/>
             <p className="text-lg font-semibold text-pretty">{article.title}</p>
             <p>{new Date(article.date).toLocaleDateString()}</p>
