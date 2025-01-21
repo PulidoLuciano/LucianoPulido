b462d72cd7508a00fcb421a790e1d8a169375a7f
Luciano Pulido <lucianonicolaspulido@gmail.com>
2025-01-21 14:57:12 -0300

    Fix date format in articles

--- Frontend/src/components/forum/articleBody.tsx
+++ Frontend/src/components/forum/articleBody.tsx
@@ -7,7 +7,7 @@ export default function ArticleBody({article} : {article : Article}){
             <section className="py-4 laptop:max-w-screen-tablet m-auto w-full px-2">
                 <h1 className="text-5xl text-pretty font-semibold pb-1">{article.title}</h1>
                 <p className="text-xl font-light pb-2">{article.description}</p>
-                <p className="text-right text-gray-500">{article.date}</p>
+                <p className="text-right text-gray-500">{new Date(article.date).toLocaleDateString()}</p>
                 <img src={article.imageUrl} alt="" />
             </section>
             <section className="pb-4">
