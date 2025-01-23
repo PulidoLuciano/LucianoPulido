a4c8ea87c135e3723e4c15d0e34a67d66ef9db7f
Luciano Pulido <lucianonicolaspulido@gmail.com>
2025-01-23 12:18:29 -0300

    Fix image styles in articles

--- Frontend/src/components/forum/articles/image.tsx
+++ Frontend/src/components/forum/articles/image.tsx
@@ -1,6 +1,6 @@
 export default function Image({url, description, alt} : {url : string, description? : string, alt : string}){
-    return <article className="w-full overflow-hidden">
+    return <article className="w-full overflow-hidden mb-4 mt-2">
         <img src={url} alt={alt} className="max-w-full mx-auto"/>
-        {description ?? <p className="text-center text-lg text-gray-600 font-thin">{description}</p>}
+        {description && <p className="text-center text-lg text-gray-600 font-thin">{description}</p>}
     </article>
 }
