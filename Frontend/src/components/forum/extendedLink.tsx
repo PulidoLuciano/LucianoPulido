bb4353b29544e63ede79da360c9b91818c6ebf90
Luciano Pulido <lucianonicolaspulido@gmail.com>
2025-01-25 20:24:43 -0300

    Fix link in extended articles

--- Frontend/src/components/forum/extendedLink.tsx
+++ Frontend/src/components/forum/extendedLink.tsx
@@ -3,7 +3,7 @@ import { ArticleExtendedPreview } from "../../types";
 
 export default function ExtendedArticleLink({article} : {article : ArticleExtendedPreview}){
     return(
-        <Link href={article.url} className="tablet:grid grid-cols-[auto_auto] gap-4 p-4 bg-white rounded-3xl shadow">
+        <Link href={`/articles/${article.url}`} className="tablet:grid grid-cols-[auto_auto] gap-4 p-4 bg-white rounded-3xl shadow">
             <img src={article.imageUrl} alt={`${article.title}'s image`} className="max-h-60 aspect-video"/>
             <div className="flex flex-col gap-1">
                 <h2 className="text-2xl text-pretty">{article.title}</h2>
